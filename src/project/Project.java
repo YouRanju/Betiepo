35a268082ac328acb8e248abcce7c56166a6b8f3
Eunju/kiori <eun01300714@gmail.com>
2019-06-27 05:03:32 +0900

    create member table and project table

--- src/project/Project.java
+++ src/project/Project.java
@@ -1,23 +1,34 @@
 package project;
 
 public class Project {
-	private String projectId;
+	private int projectId;
 	private String userEmail;
 	private String title;
-	private int pjAvailable;
 	private String genre;
-	private String language;
 	private String startDate;
 	private String endDate;
+	private String language;
 	private String charge;
 	private String github;
 	private String memo;
 	private String explanation;
-	
-	public String getProjectId() {
+	private String image;
+	private String purpose;
+	private String target;
+	private String scenario;
+	private String design1;
+	private String design2;
+	private String design3;
+	private String implementImg;
+	private String implementExp;
+	private String completeImg;
+	private String study;
+	private String problem;
+	private String realization;
+	public int getProjectId() {
 		return projectId;
 	}
-	public void setProjectId(String projectId) {
+	public void setProjectId(int projectId) {
 		this.projectId = projectId;
 	}
 	public String getUserEmail() {
@@ -32,23 +43,11 @@ public class Project {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public int getPjAvailable() {
-		return pjAvailable;
-	}
-	public void setPjAvailable(int pjAvailable) {
-		this.pjAvailable = pjAvailable;
-	}
 	public String getGenre() {
 		return genre;
 	}
 	public void setGenre(String genre) {
 		this.genre = genre;
-	}
-	public String getLanguage() {
-		return language;
-	}
-	public void setLanguage(String language) {
-		this.language = language;
 	}
 	public String getStartDate() {
 		return startDate;
@@ -61,6 +60,12 @@ public class Project {
 	}
 	public void setEndDate(String endDate) {
 		this.endDate = endDate;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
 	}
 	public String getCharge() {
 		return charge;
@@ -86,5 +91,82 @@ public class Project {
 	public void setExplanation(String explanation) {
 		this.explanation = explanation;
 	}
-	
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getPurpose() {
+		return purpose;
+	}
+	public void setPurpose(String purpose) {
+		this.purpose = purpose;
+	}
+	public String getTarget() {
+		return target;
+	}
+	public void setTarget(String target) {
+		this.target = target;
+	}
+	public String getScenario() {
+		return scenario;
+	}
+	public void setScenario(String scenario) {
+		this.scenario = scenario;
+	}
+	public String getDesign1() {
+		return design1;
+	}
+	public void setDesign1(String design1) {
+		this.design1 = design1;
+	}
+	public String getDesign2() {
+		return design2;
+	}
+	public void setDesign2(String design2) {
+		this.design2 = design2;
+	}
+	public String getDesign3() {
+		return design3;
+	}
+	public void setDesign3(String design3) {
+		this.design3 = design3;
+	}
+	public String getImplementImg() {
+		return implementImg;
+	}
+	public void setImplementImg(String implementImg) {
+		this.implementImg = implementImg;
+	}
+	public String getImplementExp() {
+		return implementExp;
+	}
+	public void setImplementExp(String implementExp) {
+		this.implementExp = implementExp;
+	}
+	public String getCompleteImg() {
+		return completeImg;
+	}
+	public void setCompleteImg(String completeImg) {
+		this.completeImg = completeImg;
+	}
+	public String getStudy() {
+		return study;
+	}
+	public void setStudy(String study) {
+		this.study = study;
+	}
+	public String getProblem() {
+		return problem;
+	}
+	public void setProblem(String problem) {
+		this.problem = problem;
+	}
+	public String getRealization() {
+		return realization;
+	}
+	public void setRealization(String realization) {
+		this.realization = realization;
+	}
 }
--- src/project/ProjectDAO.java
+++ src/project/ProjectDAO.java
@@ -1,5 +1,6 @@
 package project;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -23,6 +24,7 @@ public class ProjectDAO {
 		}
 	}
 	
+	/*
 	public String getDate() {
 		String SQL = "SELECT NOW()";
 		try {
@@ -51,23 +53,57 @@ public class ProjectDAO {
 		}
 		return -1; // DB error
 	}
+	*/
 	
 	public int write(Project project) {
-		String SQL = "INSERT INTO project VALUE(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		String SQL = "INSERT INTO project(userEmail, title";
+		
+		Field[] projectList = project.getClass().getDeclaredFields();
+		int count = 0;
+		for(Field pro: projectList) 
+		{
+			pro.setAccessible(true);
+			if(!(pro.getName().equalsIgnoreCase("userEmail")
+					|| pro.getName().equalsIgnoreCase("projectId")
+					|| pro.getName().equalsIgnoreCase("title") )) 
+			{
+				SQL += ", " + pro.getName();
+				count ++;
+			}
+		}
+		
+		SQL += ") VALUE( ?, ?";
+		for(int i=0; i<count; i++)
+			SQL += ", ?";
+		SQL += ")";
+		
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
-			pstmt.setInt(1,  getNext()); // project id
-			pstmt.setString(2, project.getUserEmail()); // userEmail
-			pstmt.setString(3, project.getTitle());
-			pstmt.setInt(4, 1); // pjAvailable
-			pstmt.setString(5, project.getGenre());
-			pstmt.setString(6, project.getLanguage());
-			pstmt.setString(7, project.getStartDate());
-			pstmt.setString(8, (project.getEndDate() == null)?getDate():project.getEndDate());
-			pstmt.setString(9, project.getCharge());
-			pstmt.setString(10, project.getGithub());
-			pstmt.setString(11, project.getMemo());
-			pstmt.setString(12, project.getExplanation());
+			
+			pstmt.setString(1, project.getUserEmail()); // userEmail
+			pstmt.setString(2, project.getTitle()); // userEmail
+			pstmt.setString(3, project.getGenre()); // userEmail
+			pstmt.setString(4, project.getStartDate()); // userEmail
+			pstmt.setString(5, project.getEndDate()); // userEmail
+			pstmt.setString(6, project.getLanguage()); // userEmail
+			pstmt.setString(7, project.getCharge()); // userEmail
+			pstmt.setString(8, project.getGithub()); // userEmail
+			pstmt.setString(9, project.getMemo()); // userEmail
+			pstmt.setString(10, project.getExplanation()); // userEmail
+			pstmt.setString(11, project.getImage()); // userEmail
+			pstmt.setString(12, project.getPurpose()); // userEmail
+			pstmt.setString(13, project.getTarget()); // userEmail
+			pstmt.setString(14, project.getScenario()); // userEmail
+			pstmt.setString(15, project.getDesign1()); // userEmail
+			pstmt.setString(16, project.getDesign2()); // userEmail
+			pstmt.setString(17, project.getDesign3()); // userEmail
+			pstmt.setString(18, project.getImplementImg()); // userEmail
+			pstmt.setString(19, project.getImplementExp()); // userEmail
+			pstmt.setString(20, project.getCompleteImg()); // userEmail
+			pstmt.setString(21, project.getStudy()); // userEmail
+			pstmt.setString(22, project.getProblem()); // userEmail
+			pstmt.setString(23, project.getRealization()); // userEmail
 			
 			return pstmt.executeUpdate();
 		} catch (Exception e) {
